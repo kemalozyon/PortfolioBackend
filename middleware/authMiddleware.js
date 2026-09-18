86b0ed7fa737982276ea3cd7d94299aaed1daa11
Kemal Özyön <kemalozyon51@gmail.com>
2026-09-18 22:03:10 +0300

    Fix JWT user ID lookup in auth middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -9,7 +9,7 @@ export const protect = async (req, res, next) => {
             token = req.headers.authorization.split(" ")[1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-            req.user = await User.findById(decoded.is).select("-password")
+            req.user = await User.findById(decoded.id).select("-password")
 
             if (!req.user) return res.status(401).json({ message: "Unauthorized access" })
             next()
